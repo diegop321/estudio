99ad1fff07830954fde3f86e58035536b3aa8f5d
DIEGO FERNANDO PENA MATEUS <DIEGOFPENA@protela.local>
2025-05-29 08:10:07 -0500

    se terminan ejercicioas asta el 15

---
+++ NODE JS/EJERCICIO 15/index.js
@@ -0,0 +1,10 @@
+
+
+// const os = require('os');
+
+// console.log(os.platform());
+
+
+
+
+console.log(process.platform);
--- NODE JS/EJERCICIO 6/index.js
+++ NODE JS/EJERCICIO 6/index.js
@@ -1,6 +1,6 @@
 const mat = require('./matematicas')
 
 console.log('La resta de 3-2=' + mat.restar( 3,2))
-console.log('La resta de 3+2=' + mat.sumar(3,2))
-console.log('La resta de 3x2=' + mat.multiplicar(3,2))
-console.log('La resta de 6%2=' + mat.dividir(6,2))
+console.log('La suma de 3+2=' + mat.sumar(3,2))
+console.log('La multiplicacion de 3x2=' + mat.multiplicar(3,2))
+console.log('La divicion de 6%2=' + mat.dividir(6,2))
